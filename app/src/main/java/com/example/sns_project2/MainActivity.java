0dbd33a49fc9ba54b86c0561c7ea2a3507d31400
hsin <leehuimin13@gmail.com>
2022-11-08 15:36:37 +0900

    community

---
+++ app/src/main/java/com/example/sns_project2/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.sns_project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            myStartActivity(SignUpActivity.class);
+        } else {
+            for (UserInfo profile : user.getProviderData()) {
+                // Name, email address, and profile photo Url
+                String name = profile.getDisplayName();
+                Log.e("이름 :", "이름 :" + name);
+                if (name != null) {
+                    if (name.length() == 0) {
+                        myStartActivity(MemberinitActivity.class);
+                    }
+                }
+            }
+        }
+
+        findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.logoutButton:
+                    FirebaseAuth.getInstance().signOut();
+                    myStartActivity(SignUpActivity.class);
+                    break;
+            }
+        }
+    };
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
